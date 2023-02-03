3c8426cf5c81148c1a3538000c83d456d1ff851b
Scalante <rubendarioscalante@gmail.com>
2023-02-03 10:03:23 -0500

    Ruben Scalante (3/02/2023)

---
+++ ChatSignalR/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSignalR
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(int room, string user, string mensaje)
+        {
+            //Enviamos un mensaje de forma asincrona a un grupo determinado
+            await Clients.Group(room.ToString()).SendAsync("RecibirMensaje", user, mensaje);
+        }
+        public async Task AddToGroup(string room)
+        {
+            //Añadimos a la persona a la sala
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+        }
+    }
+}
